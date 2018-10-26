a36bea49661b60f5b9e6bab10b2637f2caa20243
Lambda Moses <dlu2@caltech.edu>
2018-10-26 16:14:23 -0700

    Unit test the app with shinytest

---
+++ tests/mytest.R
@@ -0,0 +1,179 @@
+app <- ShinyDriver$new("../", loadTimeout = 100000)
+app$snapshotInit("mytest")
+
+# Input 'dimension' was set, but doesn't have an input binding.
+# Input 'dimension' was set, but doesn't have an input binding.
+# Input 'dimension' was set, but doesn't have an input binding.
+# Input 'dimension' was set, but doesn't have an input binding.
+# Input 'dimension' was set, but doesn't have an input binding.
+# Input 'dimension' was set, but doesn't have an input binding.
+# Input 'dimension' was set, but doesn't have an input binding.
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(interactive = TRUE)
+app$setInputs(submit = "click")
+app$snapshot()
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_relayout-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+app$snapshot()
+app$setInputs(color_by = "cluster")
+app$setInputs(submit = "click", timeout_ = 1e5)
+# Input '`.clientValue-plotly_relayout-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+app$snapshot()
+app$setInputs(interactive = FALSE)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(save_plot = TRUE)
+app$snapshot()
+app$setInputs(color_by = "none")
+app$setInputs(pt_size = 2)
+app$setInputs(save_plot = FALSE)
+app$setInputs(submit = "click")
+app$setInputs(save_plot = TRUE)
+app$snapshot()
+app$setInputs(save_plot = FALSE)
+app$setInputs(alpha = 0.1)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(pt_size = 0.2)
+app$setInputs(alpha = 0.5)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(color_by = "gene")
+app$setInputs(gene = "XLOC_008048")
+app$setInputs(theme = "dark")
+app$setInputs(submit = "click", timeout_ = 1e5)
+app$snapshot()
+app$setInputs(interactive = TRUE)
+app$setInputs(submit = "click", timeout_ = 1e5)
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+app$snapshot()
+app$snapshot()
+app$setInputs(velo = TRUE)
+app$setInputs(submit = "click")
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+app$snapshot()
+app$setInputs(interactive = FALSE)
+app$setInputs(color_by = "cell_density")
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(velo = FALSE)
+app$setInputs(submit = "click")
+app$setInputs(n_bins = 200)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(n_bins = 45)
+app$setInputs(n_bins = 50)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(n_bins = 150)
+app$setInputs(interactive = TRUE)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(n_bins = 60)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(interactive = FALSE)
+app$setInputs(n_bins = 140)
+app$setInputs(n_bins = 145)
+app$setInputs(n_bins = 150)
+app$setInputs(submit = "click")
+app$setInputs(save_plot = TRUE)
+app$snapshot()
+app$setInputs(interactive = TRUE)
+app$setInputs(interactive = FALSE)
+app$setInputs(velo = TRUE)
+app$setInputs(color_by = "gene")
+app$setInputs(gene = "XLOC_008048")
+app$setInputs(submit = "click", timeout_ = 6e5)
+app$snapshot()
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(dim_reduction = "UMAP")
+app$setInputs(submit = "click", timeout_ = 6e5)
+app$snapshot()
+app$setInputs(alpha = 0.3)
+app$setInputs(submit = "click", timeout_ = 6e5)
+app$snapshot()
+app$setInputs(alpha = 1)
+app$setInputs(pt_size = 0.3)
+app$setInputs(pt_size = 0.2)
+app$setInputs(submit = "click", timeout_ = 6e5)
+app$snapshot()
+app$setInputs(pt_size = 1)
+app$setInputs(n_grid = 42)
+app$setInputs(n_grid = 82)
+app$setInputs(submit = "click", timeout_ = 6e5)
+app$snapshot()
+app$setInputs(n_grid = 43)
+app$setInputs(n_grid = 42)
+app$setInputs(n_grid = 40)
+app$setInputs(arrow_scale = 1.5)
+app$setInputs(submit = "click", timeout_ = 6e5)
+app$snapshot()
+app$setInputs(arrow_scale = 5)
+app$setInputs(submit = "click", timeout_ = 6e5)
+app$snapshot()
+app$setInputs(velo = FALSE)
+app$setInputs(save_plot = FALSE)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(dim_reduction = "tSNE")
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(color_by = "nGene")
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(dim_use_y = 3)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(dim_use_y = 2)
+app$setInputs(dim_use_x = 2)
+app$setInputs(dim_use_x = 3)
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(dim_use_x = 2)
+app$setInputs(dim_use_x = 1)
+app$setInputs(color_by = "gene")
+app$setInputs(gene = "foo")
+app$setInputs(submit = "click")
+app$snapshot()
+app$setInputs(interactive = TRUE)
+app$setInputs(velo = TRUE)
+app$setInputs(submit = "click")
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+app$snapshot()
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+# Input '`.clientValue-plotly_hover-A`' was set, but doesn't have an input binding.
+app$setInputs(interactive = FALSE)
+app$setInputs(color_by = "cell_density")
+app$setInputs(submit = "click")
+app$snapshot()
